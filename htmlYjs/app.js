731efcd29613a8ab12b5b35593fee4666704afa9
davidz <davidzn2@gmail.com>
2020-06-01 19:32:36 -0500

    Mas ejercicios de HTML + JS

--- htmlYjs/app.js
+++ htmlYjs/app.js
@@ -1,30 +1,51 @@
-var txt = document.getElementById("texto");
-console.log(txt);
-function alerta(){
-    alert('Si ')
-}
-function cambiarTexto() {
-  var txt = document.getElementById("texto");
-  txt.innerHTML = "Esto viene de JS";
-    console.log(txt);
+// var txt = document.getElementById("texto");
+// console.log(txt);
+// function alerta(){
+//     alert('Si ')
+// }
+// function cambiarTexto() {
+//   var txt = document.getElementById("texto");
+//   txt.innerHTML = "Esto viene de JS";
+//     console.log(txt);
+// }
+
+// var contadorDeClicks = 300
+// txt.innerHTML = contadorDeClicks;
+
+// function countClicks(){
+//     var txt = document.getElementById("texto");
+//     contadorDeClicks += 1;
+//     txt.innerHTML = contadorDeClicks;
+// }
+
+// function numberA(){
+//     var txt = document.getElementById("texto");
+//     var x = document.getElementById('input1').value;
+//     if (x == 2){
+//         txt.innerHTML = 'Eso es un 2'
+//     }else{
+//         txt.innerHTML = 'eso es otro numero'
+//     }
+// }
+
+var contaClicks = 0;
+var num1 = 0;
+var num2 = 0;
+var total = 0;
+function addNum(x) {
+    var dondeMeterTexto = document.getElementById("resultado");
+  contaClicks += 1;
+  if (contaClicks == 1) {
+     num1 = x;
+  } else if (contaClicks == 2) {
+     num2 = x;
+  } 
+  total = num1 + num2;
+  
+  dondeMeterTexto.innerHTML = total
 }
 
-var contadorDeClicks = 300
-txt.innerHTML = contadorDeClicks;
 
-function countClicks(){
-    var txt = document.getElementById("texto");
-    contadorDeClicks += 1;
-    txt.innerHTML = contadorDeClicks;
-}
-
-function numberA(){
-    var txt = document.getElementById("texto");
-    var x = document.getElementById('input1').value;
-    if (x == 2){
-        txt.innerHTML = 'Eso es un 2'
-    }else{
-        txt.innerHTML = 'eso es otro numero'
-    }
-}
-
+// var aquiVa = document.getElementById('id1');
+// console.log(aquiVa)
+// aquiVa.innerHTML = 'Adios'
